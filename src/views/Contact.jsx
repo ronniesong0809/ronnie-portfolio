54dcf6f0fbd234125c4733d38ccff8bb1cec5ab9
Ronnie Song <ronniesong0809@gmail.com>
2020-08-26 17:18:20 -0700

    fix: change wave

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -20,7 +20,7 @@ const InnerWave = styled.div`
     height: 40vh;
   }
   path {
-    ${waveAnimation('20s')};
+    ${waveAnimation('10s')};
   }
 `
 
@@ -30,14 +30,7 @@ const Contact = ({ children, offset }) => (
       <WaveWrapper>
         <InnerWave>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
-            <path>
-              <animate
-                attributeName="d"
-                values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                repeatCount="indefinite"
-                dur="30s"
-              />
-            </path>
+            <path />
           </svg>
         </InnerWave>
       </WaveWrapper>
